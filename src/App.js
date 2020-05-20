ecf2812af96850c8ddadd11a6fc1b2b129f00d39
robson.machado <robson.machado@mprj.mp.br>
2020-05-20 20:31:13 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
 import React, { Component } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import './styles.css'
+import * as pdf from './pdf'
 class App extends Component {
   state = {
-    stringHtml : "<p>Conteúdo dentro do estado</p>"
-  }
+    stringHtml: "<p>Conteúdo dentro do estado</p>",
+    pdf: ''
+    }
+
+    componentDidMount(){
+      console.log(pdf.conteudo());
+      this.setState({pdf: pdf.conteudo()})
+    }
 
   mudaTexto = (data) => {
-    this.setState({stringHtml: data});
+  this.setState({ stringHtml: data});
   }
 
-    render() {
-        return (
-            <div className="App">
-                <h2>Poc reactjs com ckeditor</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data={this.state.stringHtml}
-                    onInit={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                      const data = editor.getData();
-                      this.mudaTexto(data);                      
-                      console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }                    
-                />
-                <h2>HTML gerado</h2>
-                <xmp>
-                  {this.state.stringHtml}
-                </xmp>
-                
-                
-                <div ref="http://www.tjrj.jus.br/documents/10136/3067978/anexo-ii-manual-modelo-nacional-interoperabilidade.pdf" id='viewer' style={{ width: '100%', height: '100%' }}>
+  render() {
+    return (
+      <section className="container">
+        <h2>POC com React e CKEDITOR</h2>
+        <div className="left-half">
+          <CKEditor 
+            editor={ClassicEditor}
+            data={this.state.stringHtml}
+            onInit={editor => {
+              // You can store the "editor" and use when it is needed.
+              console.log('Editor is ready to use!', editor);
+            }}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              this.mudaTexto(data);
+              console.log({ event, editor, data });
+            }}
+            onBlur={(event, editor) => {
+              console.log('Blur.', editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log('Focus.', editor);
+            }}
+          />
+          <h2>HTML gerado</h2>
+        <xmp style={{width:"50px"}}>
+            {this.state.stringHtml}
+          </xmp>          
+        </div>
+        <div className="right-half">
+          <embed src={this.state.pdf}
+            style={{width:"100%", height: "100%"}} title="title">
+          </embed>
+        </div>
+      </section>
 
-                </div>
-
-                <iframe src="http://www.tjrj.jus.br/documents/10136/3067978/anexo-ii-manual-modelo-nacional-interoperabilidade.pdf" title="title">
-                  
-              </iframe>
-                
-
-            </div>
-        );
-    }
+    );
+  }
 }
 
 export default App;
